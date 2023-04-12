1cb9f3a8dbf3de6970da1d8bc67d57f681528450
Paul Schnell <paul@agilezulu.com>
2023-04-12 10:43:36 +0100

    amplify config

--- app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,21 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "pythonapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "athletefunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "medalcasemedalcaselambdalayer": {
+      "Arn": "string"
+    }
+  }
+}
