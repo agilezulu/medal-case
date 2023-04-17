17407ecd3bc17f0414e7e6da74b22a796b26e617
Paul Schnell <paul@agilezulu.com>
2023-04-17 14:09:03 +0100

    amplify update

---
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "medalcaseapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "medalcaseathlete": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "medalcasemedalcaselambdalayer": {
+      "Arn": "string"
+    }
+  }
+}
